02a3f6934467181fd67a11e21d7bb2d0df1af244
yzyear203 <yzyear@icloud.com>
2026-04-26 16:53:29 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ]
+  plugins: [react()],
+  base: './', // 👑 体验升维：强制使用相对路径，彻底解决找不到静态资源的问题
 })
