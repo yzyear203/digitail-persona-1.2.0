24e4647dad8ac82e535214cafe397cc97222dda3
yzyear203 <yzyear@icloud.com>
2026-05-10 19:28:47 +0800

    Add official sticker sheet image paths

--- src/lib/officialStickerAssets.js
+++ src/lib/officialStickerAssets.js
@@ -1,1 +1,5 @@
-PLACEHOLDER
+export const OFFICIAL_STICKER_SHEET_IMAGES = {
+  basic_reactions: '/official-stickers/basic_reactions.png',
+  social_life: '/official-stickers/social_life.png',
+  advanced_reactions: '/official-stickers/advanced_reactions.png',
+};
